eff26ed5c456f0bb4e0ec597e7640dd781c159ef
francisacosta <francisacosta.it@gmail.com>
2026-02-09 09:24:34 +0800

    feat: added model relationships

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -6,6 +6,7 @@ use Filament\Models\Contracts\HasName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Business extends Model implements HasName
 {
@@ -29,5 +30,25 @@ class Business extends Model implements HasName
     public function getFilamentName(): string
     {
         return $this->name;
+    }
+
+    public function companies(): HasMany
+    {
+        return $this->hasMany(Company::class);
+    }
+
+    public function contacts(): HasMany
+    {
+        return $this->hasMany(Contact::class);
+    }
+
+    public function deals(): HasMany
+    {
+        return $this->hasMany(Deal::class);
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
