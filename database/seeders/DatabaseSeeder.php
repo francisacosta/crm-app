67c30f9078f5d83930dab8fea9d281b790a97354
francisacosta <francisacosta.it@gmail.com>
2026-02-09 09:22:15 +0800

    refactor: update seeder for a single business

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Business;
+use App\Models\Company;
+use App\Models\Contact;
+use App\Models\Deal;
+use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,20 +20,56 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)->create();
+        $business = Business::factory()
+            ->has(User::factory(5))
+            ->create();
 
-        User::factory()->create([
+        $superAdmin = User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
 
-        $this->call([
-            BusinessSeeder::class,
-            CompanySeeder::class,
-            ContactSeeder::class,
-            DealSeeder::class,
-            TaskSeeder::class,
-            ActivitySeeder::class,
-        ]);
+        $users = $business->users;
+
+        $business->users()->attach($superAdmin);
+
+        $companies = Company::factory(100)
+            ->for($business)
+            ->state(fn () => ['user_id' => $users->random()->id])
+            ->create();
+
+        $contacts = Contact::factory(200)
+            ->for($business)
+            ->recycle($companies)
+            ->state(fn () => ['user_id' => $users->random()->id])
+            ->create();
+
+        $deals = Deal::factory(100)
+            ->for($business)
+            ->state(function () use ($contacts, $users) {
+                $contact = $contacts->random();
+
+                return [
+                    'company_id' => $contact->company_id,
+                    'contact_id' => $contact->id,
+                    'user_id' => $users->random()->id,
+                ];
+            })
+            ->create();
+
+        $tasks = Task::factory(100)
+            ->for($business)
+            ->state(function () use ($contacts, $users, $deals) {
+                $contact = $contacts->random();
+
+                return [
+                    'company_id' => $contact->company_id,
+                    'deal_id' => $deals->random()->id,
+                    'contact_id' => $contact->id,
+                    'assigned_to' => $users->random()->id,
+                    'created_by' => $users->random()->id,
+                ];
+            })
+            ->create();
     }
 }
