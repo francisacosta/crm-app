a3042d76c6ea6bab39e9385b81eccf89607c38e9
francisacosta <francisacosta.it@gmail.com>
2026-02-14 05:27:39 +0800

    refactor: enhance ContactsTable with additional toggleable columns and filters

--- app/Filament/Resources/Contacts/Tables/ContactsTable.php
+++ app/Filament/Resources/Contacts/Tables/ContactsTable.php
@@ -6,7 +6,9 @@ use Filament\Actions\BulkActionGroup;
 use Filament\Actions\CreateAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class ContactsTable
@@ -14,24 +16,34 @@ class ContactsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultGroup('company.name')
             ->columns([
                 TextColumn::make('first_name')
                     ->searchable()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(),
                 TextColumn::make('last_name')
                     ->searchable()
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(),
                 TextColumn::make('email')
-                    ->searchable(),
-                TextColumn::make('phone'),
-                TextColumn::make('job_title'),
+                    ->searchable()
+                    ->toggleable(),
+                TextColumn::make('phone')
+                    ->toggleable(),
+                TextColumn::make('job_title')
+                    ->toggleable(),
                 TextColumn::make('company.name')
-                    ->sortable(),
+                    ->sortable()
+                    ->toggleable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('company_id')
+                    ->relationship('company', 'name')
+                    ->label('Company'),
             ])
             ->recordActions([
+                ViewAction::make(),
                 EditAction::make(),
             ])
             ->toolbarActions([
